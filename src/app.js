14e88a9de01c64fbbd8cb55c7372f5e537e859f9
dialloth <althierno@yahoo.fr>
2022-04-27 11:49:07 +0200

    connect db

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const db = require('./db');
 const app = express();
 
 const items = [
@@ -7,9 +7,24 @@ const items = [
   { id: 2, name: 'item2' },
 ];
 
+app.get('/', (req, res) => res.send('Hello Thierno'));
+
 app.get('/things', (req, res) => {
   console.log('handling GET /things');
   res.send(items);
 });
 
+// FAIRE AFFICHER TOUS LES PRODUITS
+app.get('/quotes', async (req, res) => {
+  try {
+    const [quotes] = await db.promise().query('SELECT * FROM quotes');
+    res.send(quotes);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('something wrong happened');
+  }
+});
+db.connect((err) => {
+  if (err) console.error('error connecting to db');
+});
 module.exports.app = app;
