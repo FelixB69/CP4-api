1480513d347b43586b18fbd53b1acd38ee26b3c9
dialloth <althierno@yahoo.fr>
2022-04-27 12:13:02 +0200

    add quotes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const db = require('./db');
+const Joi = require('joi');
 const app = express();
+app.use(express.json());
 
 const items = [
   { id: 1, name: 'item1' },
@@ -22,6 +24,32 @@ app.get('/quotes', async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).send('something wrong happened');
+  }
+});
+
+// AJOUTER UN PRODUIT AVEC POST
+app.post('/quotes', async (req, res) => {
+  try {
+    const { name, comment } = req.body;
+    const { error: validationErrors } = Joi.object({
+      name: Joi.string().max(50).required(),
+      comment: Joi.string().min(0).required(),
+    }).validate({ name, comment }, { abortEarly: false });
+
+    if (validationErrors) {
+      return res.status(422).json({ errors: validationErrors.details });
+    }
+    const [{ insertId }] = await db
+      .promise()
+      .query('INSERT INTO quotes (name, comment) VALUES (?, ?)', [
+        name,
+        comment,
+      ]);
+
+    res.status(201).send({ id: insertId, name, comment });
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
   }
 });
 db.connect((err) => {
