dcaa1a9f57756ccd603065b03f2f3878fd313101
Félix <fb.felixberger@gmail.com>
2022-05-03 17:28:25 +0200

    add routes

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./db');
 const Joi = require('joi');
+const connection = require('./db');
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// FAIRE AFFICHER TOUS LES PRODUITS
+// FAIRE AFFICHER LES QUOTES
 app.get('/quotes', async (req, res) => {
   try {
     const [quotes] = await db.promise().query('SELECT * FROM quotes');
@@ -17,7 +18,25 @@ app.get('/quotes', async (req, res) => {
   }
 });
 
-// AJOUTER UN PRODUIT AVEC POST
+app.get('/quotes/:id', (req, res) => {
+  const quotesId = req.params.id;
+  connection.query(
+    'SELECT * FROM quotes WHERE id = ?',
+    [quotesId],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error retrieving quote from database');
+      } else if (result.length === 0) {
+        res.status(404).send('Quote not found');
+      } else {
+        res.json(result[0]);
+      }
+    }
+  );
+});
+
+// AJOUTER UNE QUOTE AVEC POST
 app.post('/quotes', async (req, res) => {
   try {
     const { name, comment } = req.body;
@@ -42,6 +61,93 @@ app.post('/quotes', async (req, res) => {
     res.sendStatus(500);
   }
 });
+// SUPPRIMER UNE QUOTE AVEC DELETE
+
+app.delete('/quotes/:id', (req, res) => {
+  const quotesId = req.params.id;
+  connection.query(
+    'DELETE FROM quotes WHERE id = ?',
+    [quotesId],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error deleting a quote');
+      } else {
+        res.sendStatus(204);
+      }
+    }
+  );
+});
+
+// FAIRE AFFICHER LES GIFS
+app.get('/gifs', async (req, res) => {
+  try {
+    const [gifs] = await db.promise().query('SELECT * FROM gifs');
+    res.send(gifs);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('something wrong happened');
+  }
+});
+
+app.get('/gifs/:id', (req, res) => {
+  const gifsId = req.params.id;
+  connection.query(
+    'SELECT * FROM gifs WHERE id = ?',
+    [gifsId],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error retrieving quote from database');
+      } else if (result.length === 0) {
+        res.status(404).send('Quote not found');
+      } else {
+        res.json(result[0]);
+      }
+    }
+  );
+});
+
+// AJOUTER UN GIF AVEC POST
+app.post('/gifs', async (req, res) => {
+  try {
+    const { name, gif } = req.body;
+    const { error: validationErrors } = Joi.object({
+      name: Joi.string().max(50).required(),
+      gif: Joi.string().min(0).required(),
+    }).validate({ name, gif }, { abortEarly: false });
+
+    if (validationErrors) {
+      return res.status(422).json({ errors: validationErrors.details });
+    }
+    const [{ insertId }] = await db
+      .promise()
+      .query('INSERT INTO quotes (name, gif) VALUES (?, ?)', [name, gif]);
+
+    res.status(201).send({ id: insertId, name, gif });
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+});
+// SUPPRIMER UN GIF AVEC DELETE
+
+app.delete('/gifs/:id', (req, res) => {
+  const gifsId = req.params.id;
+  connection.query(
+    'DELETE FROM quotes WHERE id = ?',
+    [gifsId],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error deleting a quote');
+      } else {
+        res.sendStatus(204);
+      }
+    }
+  );
+});
+
 db.connect((err) => {
   if (err) console.error('error connecting to db');
 });
